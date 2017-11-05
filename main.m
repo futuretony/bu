b6c7fafeef634494e4bb43e7077f5e43f499f8e8
sherwinhlee <sherwin.h.lee@gmail.com>
2017-11-04 21:02:04 -0400

    Replace | with comma for city req. param

--- main.m
+++ main.m
@@ -14,7 +14,7 @@ cities = cell(1,length(cityData));
 
 % Loop to store weather data in cell array
 for i = 1:length(cityData)
-    q = cityData{i};
+    q = strrep(cityData{i},'|',',');
     city = strtok(cityData{i},'|');
     fprintf('Retrieving data for %s...\n',city)
     cities{i} = webread(endpoint,'q',q,'appid',appid);
