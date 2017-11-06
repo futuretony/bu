a09bb40587b05a1d11f5bb8776bfff02c4e29c82
sherwinhlee <sherwin.h.lee@gmail.com>
2017-11-06 17:50:16 -0500

    Add units as parameter and include new plot func

--- main.m
+++ main.m
@@ -7,7 +7,7 @@ cityData={'Timbuktu|ml','Boston|us','Beijing|cn','Cape Town|za',...
 
 % Endpoint URL and API Key
 endpoint = 'https://api.openweathermap.org/data/2.5/forecast';
-appid = '70e9f383955e7372c06948520cd38387';
+appid = '3b5c47f50ea86a5973a03f00ce01b2f2';
 
 % Preallocate cell array to store weather data
 cities = cell(1,length(cityData));
@@ -17,11 +17,10 @@ for i = 1:length(cityData)
     q = strrep(cityData{i},'|',',');
     city = strtok(cityData{i},'|');
     fprintf('Retrieving data for %s...\n',city)
-    cities{i} = webread(endpoint,'q',q,'appid',appid);
+    cities{i} = webread(endpoint,'q',q,'appid',appid,...
+                        'units','imperial');
 end
 
-% Function to plot line graph of individual city temp data
-% hourtempplot(cities)
-
-% Function to scatter plot each city's avg max temp
-% avgmaxtempplot(cities)
+% Function to scatter plot each city's avg 5 day max temp
+% and line graph of individual city's 3-hour temp data
+plot_it(cities)
